cc9b913d6b797f989aba994fde4fd4ef5cbd3d25
jiyeonBaek <wldus3429@hanmail.net>
2015-11-16 13:49:07 +0900

    151116flask
    flask

---
+++ 151116flask/151116flask/_151116flask.py
@@ -0,0 +1,38 @@
+﻿from flask import Flask,url_for,redirect
+app = Flask(__name__)
+
+@app.route('/profile/<username>')
+def get_profile(username):
+    return 'profile : '+username
+
+@app.route('/hello')
+def hello_world():
+    #return 'hello world!'
+    return redirect(url_for('login'))
+
+@app.route('/message/<int:message_id>')
+def get_message(message_id):
+    return 'message_id : %d'%message_id
+
+#with app.test_request_context():
+#    print(url_for('get_profile',username='greenjoa'))
+
+@app.route('/login')
+def login():
+    return 'login'
+
+#@app.route('/profile/', methods=['POST','GET'])
+#def profile(username=None):
+#    error=None
+#    if request.method == 'POST':
+#        username = request.form['username']
+#        email = request.form['email']
+#        if not username and not email:
+#            return add_profile(request.form)
+#        else:
+#            error = 'Invalide username or email'
+#        return render_template('profile.html', error=error)
+
+
+if __name__ == '__main__':    app.run()
+    #app.debug=True #소스 변경과 디버거 제공 , 에러 원인 다 보여줌    #app.run(host="203.252.166.31")
