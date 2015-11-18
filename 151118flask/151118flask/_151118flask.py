14e6f7958dbcc6f1dfbb9fbbfda70a2bfc6d4233
jiyeonBaek <wldus3429@hanmail.net>
2015-11-18 14:22:08 +0900

    151118flask
    f

---
+++ 151118flask/151118flask/_151118flask.py
@@ -0,0 +1,26 @@
+﻿from flask import Flask,url_for,redirect,render_template
+
+app = Flask(__name__)
+
+@app.route('/hello/')
+#@app.route('/hello/<name>') # 경로 여러개에 함수 하나 매핑 가능
+def hello():
+    #return render_template('hello.html', name=name)
+    data = [dict(href="http://www.naver.com",caption="네이버"), dict(href="http://www.google.com",caption="구글")]
+    return render_template('hello.html',items=data)
+
+@app.route('/')
+def hello_world():
+    data={
+        'title' : 'Hello',
+        'name' : 'greenjoa'
+        }
+    return render_template('first.html', **data)
+
+@app.route('/main/')
+def callMain():
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
